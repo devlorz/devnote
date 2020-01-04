478e7b77efc109a8586e797be5fb978f73243e18
Nutti Saelor <leelorz6@gmail.com>
2020-01-04 17:37:56 +0700

    add navbar

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -63,10 +63,10 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  */
 
 /***************************************************************************************************
-  
+
 * SCULLY IMPORTS
-  
+
 */
-  
-// tslint:disable-next-line: align 
+
+// tslint:disable-next-line: align
 import 'zone.js/dist/task-tracking';
