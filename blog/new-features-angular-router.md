0c8eb7f0908b365cbf6f70850ca68cd21ee26ea0
Nutti Saelor <leelorz6@gmail.com>
2020-01-25 01:47:18 +0700

    add blog

---
+++ blog/new-features-angular-router.md
@@ -0,0 +1,7 @@
+---
+title: new-features-angular-router
+description: blog description
+publish: false
+---
+
+# new-features-angular-router
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import {IdleMonitorService} from '@scullyio/ng-lib';
-import { Component } from '@angular/core';
+import { IdleMonitorService, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
- constructor(private idle: IdleMonitorService) { }
-
+export class AppComponent implements OnInit {
   title = 'devnote';
+
+  constructor(
+    private idle: IdleMonitorService,
+    public scully: ScullyRoutesService
+  ) {}
+
+  ngOnInit() {
+    this.scully.available$.subscribe(res => console.log(res));
+    this.scully.topLevel$.subscribe(res => console.log(res));
+  }
 }
