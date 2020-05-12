1bbf8bcb1b8092dc39b9ba53ab19b27fd983baff
Nutti <nutti@7peakssoftware.com>
2020-05-12 21:45:10 +0700

    fix some style

--- src/app/components/article-list/article-list.component.ts
+++ src/app/components/article-list/article-list.component.ts
@@ -1,15 +1,15 @@
-import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { ScullyRoutesService } from '@scullyio/ng-lib';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { Article } from 'src/app/interface';
-import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-article-list',
   templateUrl: './article-list.component.html',
-  styleUrls: ['./article-list.component.scss']
+  styleUrls: ['./article-list.component.scss'],
 })
 export class ArticleListComponent implements OnInit {
   list$: Observable<Article[]>;
@@ -28,24 +28,26 @@ export class ArticleListComponent implements OnInit {
     this.meta.updateTag({ name: 'twitter:title', content: 'DevNote' });
     this.meta.updateTag({
       name: 'description',
-      content: 'DevNote - Brief story for developers'
+      content: 'DevNote - Brief story for developers',
     });
     this.meta.updateTag({
       name: 'og:description',
-      content: 'DevNote - Brief story for developers'
+      content: 'DevNote - Brief story for developers',
     });
     this.meta.updateTag({
       name: 'twitter:description',
-      content: 'DevNote - Brief story for developers'
+      content: 'DevNote - Brief story for developers',
     });
     this.meta.updateTag({ name: 'author', content: 'DevNote' });
   }
 
   ngOnInit() {
     this.list$ = this.scully.available$.pipe(
+      tap((res) => console.log(res)),
       map((items: Article[]) =>
         items
-          .filter(item => item.route !== '/')
+          .filter((item) => item.published)
+          .filter((item) => item.route !== '/')
           .sort((a, b) => {
             const dateA = new Date(a.date);
             const dateB = new Date(b.date);
